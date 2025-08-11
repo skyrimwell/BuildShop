6910a559fd35d504277c5ad18119f527f9d230f6
skyrimwell <51721698+skyrimwell@users.noreply.github.com>
2025-08-11 16:51:26 +0300

    PurchaseMenu + Outlay working

---
+++ Source/BuildStore/Public/MapWorkerUtils.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MapWorkerUtils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDSTORE_API UMapWorkerUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
