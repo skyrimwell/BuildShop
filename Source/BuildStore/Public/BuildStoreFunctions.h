3e0f22c75d12b3809c6ce5b5a8d54bf446b63042
kivvchanski <132572264+kivvchanski@users.noreply.github.com>
2025-08-13 17:33:54 +0300

    Trying to learn c++

---
+++ Source/BuildStore/Public/BuildStoreFunctions.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BuildStoreFunctions.generated.h"
+#include "ST_Itemstack.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDSTORE_API UBuildStoreFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category="BuildStoreFunctions")
+	static 
+	
+};
